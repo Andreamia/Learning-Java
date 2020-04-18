69ae5030b5531926c0492e6ff1f523021d1e1866
Lada Bratsykhina <spirirted.away@gmail.com>
2020-04-18 15:41:21 +0300

    final version of guessing game

--- GuessingGame/src/GuessingGame.java
+++ GuessingGame/src/GuessingGame.java
@@ -1,3 +1,7 @@
+//Desktop game where user try to guess the number from 1 to 100 in limited number tries.
+//The guessed number is generated randomly.
+//Swing is used
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -10,29 +14,36 @@ import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-
 public class GuessingGame extends JFrame {
 
-	private JTextField txtGuess;
-	private JLabel lblOutput;
-	private int theNumber;
-	private int numberOfTries;
+	private int theNumber;				// guessed number that user tries to guess
+	private int numberOfTriesMax = 7; 	// maximum number of user's tries to guess the number
+	private int numberOfTries; 			// remaining number of user's tries to guess the number
+	
+	private JTextField txtGuess;		// text field for entering number
+	private JLabel lblOutput;			// label with info for user
+	private JButton btnGuess;			// button for guessing
+	private JButton btnPlayAgain;		// button for playing new game
 
+	// check if entered number equal to guessed number
 	public void checkGuess() {
 
-		String guessText = txtGuess.getText();
+		String guessText = txtGuess.getText();		// read text with entered number
 		String message = "";
 		numberOfTries--;
+		
 		try {
-			int guess = Integer.parseInt(guessText);
+			int guess = Integer.parseInt(guessText); // parse entered number from text
 
+			//compare entered number with guessed number
 			if (guess < theNumber) {
 				message = guess + " is too low. ";
 				if (numberOfTries > 0)
 					message += "Try again!";
 				else {
-					message += "You used 7 tries. You lose! Let's play again!";
-					newGame();
+					message += "You used " + numberOfTriesMax + " tries. You lose! Let's play again!";
+					btnGuess.setVisible(false);
+					btnPlayAgain.setVisible(true);
 				}
 			}
 			else if (guess > theNumber) {
@@ -40,19 +51,27 @@ public class GuessingGame extends JFrame {
 				if (numberOfTries > 0)
 					message += "Try again!";
 				else {
-					message += "You used 7 tries. You lose! Let's play again!";
-					newGame();
+					message += "You used " + numberOfTriesMax + " tries. You lose! Let's play again!";
+					btnGuess.setVisible(false);
+					btnPlayAgain.setVisible(true);
 				}
 			}
 			else {
-				message = guess + " is correct. You win after " + (7-numberOfTries)+ " tries! Let's play again!";
-				newGame();
+				message = guess + " is correct! You win after " + (numberOfTriesMax - numberOfTries) + 
+						" tries! Let's play again!";
+				btnGuess.setVisible(false);
+				btnPlayAgain.setVisible(true);
 			}
 
 
 
 		} catch (Exception e) {
-			message = "Enter a whole number between 1 and 100!";
+			message = "Incorrect value! Enter a whole number between 1 and 100! ";
+			if (numberOfTries <= 0) {
+				message = "You used " + numberOfTriesMax + " tries. You lose! Let's play again!";
+				btnGuess.setVisible(false);
+				btnPlayAgain.setVisible(true);
+			}
 		} finally {
 			lblOutput.setText(message);
 			txtGuess.requestFocus();
@@ -60,11 +79,14 @@ public class GuessingGame extends JFrame {
 		}
 	}
 
+	//start new guessing game
 	public void newGame() {
-		theNumber = (int)(Math.random()*100 + 1);
-		numberOfTries = 7;
+		theNumber = (int)(Math.random()*100 + 1); 	// generate guessed number randomly
+		numberOfTries = numberOfTriesMax;			// set number of user's tries to maximum for new game
+		btnPlayAgain.setVisible(false);
 	}
 
+	//guessing game's window
 	public GuessingGame() {
 
 		setForeground(Color.WHITE);
@@ -76,48 +98,67 @@ public class GuessingGame extends JFrame {
 		JLabel lblTitle = new JLabel("Lada's Hi-Lo Guessing Game");
 		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTitle.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
-		lblTitle.setBounds(1, 33, 434, 28);
+		lblTitle.setBounds(1, 24, 434, 28);
 		getContentPane().add(lblTitle);
 
 		JLabel lblGuess = new JLabel("Guess a number between 1 and 100:");
 		lblGuess.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblGuess.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
-		lblGuess.setBounds(1, 94, 301, 21);
+		lblGuess.setBounds(1, 76, 301, 21);
 		getContentPane().add(lblGuess);
 
+		//text field for entering number
 		txtGuess = new JTextField();
+		txtGuess.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
+		txtGuess.setBounds(305, 76, 51, 20);
+		getContentPane().add(txtGuess);
+		txtGuess.setColumns(10);
 		txtGuess.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				checkGuess();
 			}
 		});
-		txtGuess.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
-		txtGuess.setBounds(309, 94, 51, 20);
-		getContentPane().add(txtGuess);
-		txtGuess.setColumns(10);
 
-		JButton btnGuess = new JButton("Guess!");
+		//button for guessing
+		btnGuess = new JButton("Guess!");
+		btnGuess.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
+		btnGuess.setBounds(172, 121, 89, 23);
+		getContentPane().add(btnGuess);
 		btnGuess.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				checkGuess();
 			}
 		});
-		btnGuess.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
-		btnGuess.setBounds(172, 148, 89, 23);
-		getContentPane().add(btnGuess);
 
+		//label with info for user
 		lblOutput = new JLabel("Enter a number above and click Guess!");
 		lblOutput.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
 		lblOutput.setHorizontalAlignment(SwingConstants.CENTER);
-		lblOutput.setBounds(0, 204, 434, 21);
+		lblOutput.setBounds(1, 168, 434, 21);
 		getContentPane().add(lblOutput);
+		
+		//button for playing new game
+		btnPlayAgain = new JButton("Play Again");
+		btnPlayAgain.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
+		btnPlayAgain.setBounds(167, 213, 100, 23);
+		getContentPane().add(btnPlayAgain);
+		btnPlayAgain.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				btnGuess.setVisible(true);
+				lblOutput.setText("Enter a number above and click Guess!");
+				newGame();
+			}
+		});
+		btnPlayAgain.setVisible(false);
 	}
 
+	// program execution
 	public static void main(String[] args) {
 
-		GuessingGame theGame = new GuessingGame();
-		theGame.newGame();
-		theGame.setSize(new Dimension(450,300));
-		theGame.setVisible(true);
+		GuessingGame theGame = new GuessingGame();	// create guessing game's window
+		theGame.setSize(new Dimension(450,300));	
+		theGame.setVisible(true);					
+		
+		theGame.newGame();							// start new guessing game
 	}
 }
